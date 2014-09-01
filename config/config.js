d5b3820366f8a9af80107af8a41110c4eccbde05
johnHackworth <javieralvarezlop@gmail.com>
2014-09-01 09:30:38 +0200

    damages

--- config/config.js
+++ config/config.js
@@ -1,5 +1,10 @@
 window.boot = window.boot || {};
 window.boot.config = window.boot.config || {};
 window.boot.config.width = window.innerWidth; // 1200;
+if (window.boot.config.width < 1000) {
+  window.boot.config.width = 1000;
+}
 window.boot.config.height = window.innerHeight; //800;
-window.boot.config.tileWidth = 50;
+if (window.boot.config.height < 800) {
+  window.boot.config.height = 720;
+}
