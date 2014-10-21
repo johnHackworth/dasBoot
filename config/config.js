7983831b151db655ddffa0ca86fee570637890e0
johnHackworth <javieralvarezlop@gmail.com>
2014-10-21 08:56:15 +0200

    underwater ship list

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ window.boot.config.width = 1200; //window.innerWidth; // 1200;
 // if (window.boot.config.width < 1000) {
 //   window.boot.config.width = 1000;
 // }
-window.boot.config.height = 600; //window.innerHeight; //800;
+window.boot.config.height = 700; //window.innerHeight; //800;
 // if (window.boot.config.height < 600) {
 //   window.boot.config.height = 720;
 // }
